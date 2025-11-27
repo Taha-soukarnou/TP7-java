4c818f688099d2e641ada35fd42e2f5eeefe5909
Taha-soukarnou <soukarnoutaha@gmail.com>
2025-11-27 12:49:35 +0100

    TP7

---
+++ src/exercice1/EmployeSalarie.java
@@ -0,0 +1,16 @@
+package exercice1;
+
+public class EmployeSalarie extends Employe {
+    protected double salaireMensuel;
+
+    public EmployeSalarie(String nom, String prenom,
+                          double salaireMensuel) {
+        super(nom, prenom);
+        this.salaireMensuel = salaireMensuel;
+    }
+
+    @Override
+    public double calculerSalaire() {
+        return salaireMensuel;
+    }
+}
---
+++ src/exercice2/FsItem.java
@@ -0,0 +1,20 @@
+package exercice2;
+
+public abstract class FsItem {
+    protected String name;
+
+    public FsItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract long getSize();
+
+    @Override
+    public String toString() {
+        return name + " (" + getSize() + " B)";
+    }
+}
